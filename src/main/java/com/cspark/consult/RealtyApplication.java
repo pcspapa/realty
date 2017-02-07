f32440bbbcbc8dd70f82df50248637a5fd40c392
pcspapa <pcspapa@gmail.com>
2017-02-07 11:27:12 +0900

    Initial commit

---
+++ src/main/java/com/cspark/consult/RealtyApplication.java
@@ -0,0 +1,12 @@
+package com.cspark.consult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtyApplication.class, args);
+	}
+}
